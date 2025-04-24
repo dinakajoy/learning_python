4c7356e72a595a026750a2c7251edef2eb234562
dinakajoy <odinakajoy909@gmail.com>
2025-04-24 13:09:39 +0100

    python learning and practice

--- practice.py
+++ practice.py
@@ -17,6 +17,7 @@ for row in picture:
         else:
             print(empty, end='')
     print(' ')  # Add a new line after every row
+
 # New lines before next excercise
 print(' ')
 print(' ')
@@ -24,6 +25,7 @@ print(' ')
 
 # Print out duplicates in a list
 my_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+# method 1
 unique = []
 duplicates = []
 for item in my_list:
@@ -33,15 +35,16 @@ for item in my_list:
         unique.append(item)
 print(duplicates)
 print(' ')
-# Same - Using comprehensions
+# # method 2 - Using comprehensions
 comp_duplicates = list(set([item for item in my_list if my_list.count(item) > 1]))
 print(comp_duplicates)
+
 # New lines before next excercise
 print(' ')
 print(' ')
 
 
-# Print hihest even number
+# Print highest even number
 def highest_even(lists):
     highest_even_number = 0
     for number in lists:
@@ -49,6 +52,7 @@ def highest_even(lists):
             highest_even_number = number
     return highest_even_number
 print(highest_even([10, 2, 3, 4, 8, 28, 11]))
+
 # New lines before next excercise
 print(' ')
 print(' ')
